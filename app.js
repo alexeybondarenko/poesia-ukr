214908f401d10cb6084bf8bca79f316498a2f5f2
Alexey Bondarenko <alexeybondarenko@me.com>
2015-11-15 19:00:08 +0200

    turn off add router

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ app.get('/', function (req, res) {
     title: 'Головна'
   })
 });
-
-app.get('/add', function (req, res) {
-  res.render('add', {
-    title: 'Додати'
-  });
-});
+//
+//app.get('/add', function (req, res) {
+//  res.render('add', {
+//    title: 'Додати'
+//  });
+//});
 
 /**
  * API Authors
